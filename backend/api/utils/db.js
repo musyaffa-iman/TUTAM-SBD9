415fa64e99a53720bd628faedf0195a969bdb5bb
musyaffa-iman <musyaffa.iman@gmail.com>
2025-05-04 22:44:01 +0700

    backend serverless attempt

--- backend/api/utils/db.js
+++ backend/api/utils/db.js
@@ -16,7 +16,7 @@ async function connectDB() {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose.connect(process.env.MONGODB_URI, opts).then((mongoose) => {
+    cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
       return mongoose;
     });
   }
